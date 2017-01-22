ed12c005b4f7fcfdb115c223ce97ce7302cf6c6f
Brian <brnli7@gmail.com>
2017-01-22 15:43:50 -0500

    Added FCM support

--- app/src/main/java/cwru/edu/hackcwru/firebase/FirebaseService.java
+++ app/src/main/java/cwru/edu/hackcwru/firebase/FirebaseService.java
@@ -3,6 +3,7 @@ package cwru.edu.hackcwru.firebase;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
 
+import cwru.edu.hackcwru.R;
 import cwru.edu.hackcwru.server.HackCWRUServerCalls;
 import cwru.edu.hackcwru.utils.Log;
 import retrofit2.Call;
@@ -22,18 +23,17 @@ public class FirebaseService extends FirebaseInstanceIdService {
         sendRegistrationToServer(refreshedToken);
     }
 
-    private void sendRegistrationToServer(String token){
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(HackCWRUServerCalls.API_BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-        HackCWRUServerCalls hackCWRUServerCalls = retrofit.create(HackCWRUServerCalls.class);
+    private void sendRegistrationToServer(String deviceToken){
+        HackCWRUServerCalls hackCWRUServerCalls = getHackCWRUServerCalls();
 
-        Call<DeviceTokenResponse> registerDevice = hackCWRUServerCalls.registerDevice(token);
+        Call<DeviceTokenResponse> registerDevice = hackCWRUServerCalls.registerDevice(getString(R.string.HACKCWRU_API_KEY),
+                getString(R.string.operating_system),
+                deviceToken);
         registerDevice.enqueue(new Callback<DeviceTokenResponse>() {
             @Override
             public void onResponse(Call<DeviceTokenResponse> call, Response<DeviceTokenResponse> response) {
-                Log.d(LOG_TAG, response.toString());
+                DeviceTokenResponse deviceTokenResponse = response.body();
+                Log.d(LOG_TAG, deviceTokenResponse.toString());
             }
 
             @Override
@@ -41,5 +41,13 @@ public class FirebaseService extends FirebaseInstanceIdService {
                 Log.d(LOG_TAG, "Error Registering Device");
             }
         });
+    }
+
+    private HackCWRUServerCalls getHackCWRUServerCalls(){
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(HackCWRUServerCalls.API_BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        return retrofit.create(HackCWRUServerCalls.class);
     }
 }
